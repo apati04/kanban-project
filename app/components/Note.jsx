710814441118e487b5f5ce43220e824a92ec915c
Andrew Patipaksiri <apax714@gmail.com>
2017-03-06 00:09:38 -0800

    setup drag capability inside Note component

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { DragSource } from 'react-dnd';
+import ItemTypes from '../constants/ItemTypes';
 
-
-export default ({children, ...props}) => (
-  <div {...props}>
-    {children}
-  </div>
-);
+const Note = ({ connectDragSource, children, ...props }) => {
+  return connectDragSource(
+    <div {...props}>
+      { children }
+    </div>
+  );
+}
+const noteSource = {
+  beginDrag(props) {
+    console.log('begin note drag', props);
+    return {};
+  }
+};
+export default DragSource(ItemTypes.NOTE, noteSource, connect => ({
+  connectDragSource: connect.dragSource()
+}))(Note);

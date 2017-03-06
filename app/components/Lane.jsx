36d9acb5290d6b5f7984ae5e21b0106bafe805f5
Andrew Patipaksiri <apax714@gmail.com>
2017-03-05 16:11:18 -0800

    refactor Lane Component to a container component

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import uuid from 'uuid';
+import connect from '../libs/connect';
+import NoteActions from '../actions/NoteActions';
+import Notes from './Notes';
 
-export default ({lane, ...props}) => (
-  <div {...props}>{lane.name}</div>
-)
+const Lane = ({
+  lane, notes, NoteActions, ...props
+}) => {
+  const editNote = (id, task) => {
+    NoteActions.update({id, task, editing: false});
+  };
+  const addNote = e => {
+    const noteId = uuid.v4();
+    NoteActions.create({
+      id:noteId,
+      task: 'New task'
+    });
+  }
+  const deleteNote = (noteId, e) => {
+    e.stopPropagation();
+    NoteActions.delete(noteId);
+  };
+  const activateNoteEdit = id => {
+    NoteActions.update({id, editing: true});
+  };
+}

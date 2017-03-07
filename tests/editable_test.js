db300da7ecb30309b9f29a2f67d9f98468ae87b5
Andrew Patipaksiri <apax714@gmail.com>
2017-03-07 14:03:24 -0800

    import dependencies for react-dnd testing

--- tests/editable_test.js
+++ tests/editable_test.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import assert from 'assert';
+import {
+  renderIntoDocument,
+  findRenderedDOMComponentWithTag,
+  Simulate
+} from 'react-addons-test-utils';
 import Editable from '../app/components/Editable';
 
 describe('Editable', function(){
@@ -8,4 +13,28 @@ describe('Editable', function(){
     const component = new Editable({value});
     assert.equal(component.props.children, value);
   });
+
+  it('triggers onEdit through the DOM', function() {
+    let triggered = false;
+    const newValue = 'value';
+    const onEdit = (val) => {
+      triggered = true;
+      assert.equal(val, newValue);
+    };
+    const component = renderIntoDocument(
+      <Wrapper>
+        <Editable editing={true} value={'value'} onEdit={onEdit} />
+      </Wrapper>
+    );
+    const input = findRenderedDOMComponentWithTag(component, 'input');
+    input.value = newValue;
+    Simulate.blur(input);
+    assert.equal(triggered, true);
+  });
+
 });
+class Wrapper extends React.Component {
+  render() {
+    return <div>{this.props.children}</div>;
+  }
+}
---
+++ tests/note_test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+  renderIntoDocument
+} from 'react-addons-test-utils';
+import TestBackend from 'react-dnd-test-backend';
+import {DragDropContext} from 'react-dnd';
+import assert from 'assert';
+import Note from '../app/componenets/Note';
+
+describe('Note', function() {
+  it('renders children', function())
+})
